169119707873fbcb7faaace27cda5dcf852e6191
Israel Nunes <israelnetonunes@gmail.com>
2024-11-01 16:27:38 -0300

    Exclusão do sctipt para scroll do cabeçalho - ajustes na landing page para cabeçalho não descer junto com scroll

--- public/JS/index.js
+++
@@ -1,9 +0,0 @@
-
-window.addEventListener('scroll', function() {
-    const navbar = document.getElementById('navbar');
-    if (window.scrollY > 50) {  // Quando o usuário rolar mais de 50px
-        navbar.classList.add('bg-white', 'bg-opacity-80', 'shadow-md');  // Fundo branco com opacidade
-    } else {
-        navbar.classList.remove('bg-white', 'bg-opacity-80', 'shadow-md');  // Remove fundo e sombra
-    }
-});
