f9e619b1294c4d91ba4ba21d4a592a9b5066f6ae
Israel Nunes <israelnetonunes@gmail.com>
2024-11-23 14:36:03 -0300

    Ajuste do CORS policy para a requisição da API getApiKey - Concatenação das lógicas do backend em apenas um arquivo /functions/index.js

--- functions/index.js
+++ functions/index.js
@@ -7,4 +7,19 @@ const serviceAccountKey = 'serviceAccountKey.json';
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccountKey),
     // databaseURL: `https://${process.env.PROJECT_ID}.firebaseapp.com`
+});
+
+const cors = require('cors')({origin: 'https://myprojectdictionary-9cb59.web.app'});
+
+exports.getApiKey = functions.https.onRequest((req, res) => {
+    cors(req, res, () => {
+        res.json({
+            apiKey: process.env.API_KEY,
+            authDomain: process.env.AUTH_DOMAIN,
+            projectId: process.env.PROJECT_ID,
+            storageBucket: process.env.STORAGE_BUCKET,
+            messagingSenderId: process.env.MESSAGING_SENDER_ID,
+            appId: process.env.APP_ID
+        });
+    });
 })
