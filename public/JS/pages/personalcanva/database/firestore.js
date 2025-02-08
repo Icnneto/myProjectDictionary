4516eebcdeb76ce096e2e5a7d0536a1141c2450c
Israel Nunes <israelnetonunes@gmail.com>
2025-02-08 14:19:10 -0300

    Incluir função de registrarNovoTermo

--- public/JS/pages/personalcanva/database/firestore.js
+++ public/JS/pages/personalcanva/database/firestore.js
@@ -1,18 +1,50 @@
-import { app } from "../../../firebaseConfig.js"
-import { getFirestore, collection, doc, addDoc, setDoc, getDoc, getDocs, updateDoc, onSnapshot, runTransaction } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-firestore.js";
+import { app } from "../../../firebaseConfig.js";
+import {
+  getFirestore,
+  collection,
+  doc,
+  addDoc,
+  setDoc
+} from "https://www.gstatic.com/firebasejs/11.0.2/firebase-firestore.js";
+import { criarEAcrescentarCard } from "../formCard.js";
+const user = JSON.parse(sessionStorage.getItem("userInfo"));
+const userId = user.userId;
+
 const db = getFirestore(app);
 
-async function add () {
-    try {
-        const docRef = await addDoc(collection(db, "users"), {
-          first: "Ada",
-          last: "Lovelace",
-          born: 1815
-        });
-        console.log("Document written with ID: ", docRef.id);
-      } catch (e) {
-        console.error("Error adding document: ", e);
-    }
+const userCollection = collection(db, "users");
+const termosCollection = collection(db, "termos");
+
+// const databaseRef = ref(db, `users/${userId}`);
+
+// Adiciona um novo termo ao banco de dados e atualiza a referência no nó do usuário
+async function registrarNovoTermo(listaInputs, uId) {
+  const dadosCard = {
+    termo: listaInputs.termo,
+    descricao: listaInputs.descricao,
+    favoritado: false,
+  };
+
+  try {
+    const addTerm = await addDoc(termosCollection, dadosCard);
+    const newTermKey = addTerm.id;
+
+    // user terá ID com base no seu Uid,
+    // diferentemente do Termo cujo id é criado aleatorimente pelo firestore
+    const userKey = uId;
+
+    await setDoc(doc(userCollection, userKey), {
+      termos: {
+        [newTermKey]: true
+      },
+    }, { merge: true });
+
+    return newTermKey;
+  } catch (error) {
+    console.error("Error adding document: ", error);
+    throw error;
+  }
 }
 
-// add();
+
+export { registrarNovoTermo };
--- public/JS/pages/personalcanva/formCard.js
+++ public/JS/pages/personalcanva/formCard.js
@@ -1,4 +1,4 @@
-import { registrarNovoTermo } from "./realtimeDatabase.js";
+import { registrarNovoTermo } from "./database/firestore.js";
 import { favoritarCard } from "./favoritarTermo.js";
 import { exibirModalEdicao } from "./editarTermo.js";
 import { exibirModalExclusao } from "./deletarTermo.js";
