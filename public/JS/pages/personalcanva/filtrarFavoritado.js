3317880b7acc9956c6931036e6637b20ed0ec96d
Israel Nunes <israelnetonunes@gmail.com>
2025-03-21 10:41:41 -0300

    Inserir btn de meus favoritos no menu lateral - criar diretório para inserir a lógica dos eventos do botão public/js/pages/personalcanva/filtrarFavoritado.js

---
+++ public/JS/pages/personalcanva/filtrarFavoritado.js
@@ -0,0 +1,4 @@
+import { criarEAcrescentarCard } from "../../components/cardTermo.js";
+
+const secaoListaTermos = document.querySelector('#lista-termos');
+const btnMeusFavoritos = document.querySelector('[data-filtrar_favorito]');
