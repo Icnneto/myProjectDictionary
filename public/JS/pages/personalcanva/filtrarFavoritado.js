6bc612c6fd12ae0f3afca9572614b111cf775465
Israel Nunes <israelnetonunes@gmail.com>
2025-03-22 15:56:39 -0300

    Atualizar tailwind e refatorar implementação

--- public/JS/pages/personalcanva/filtrarFavoritado.js
+++ public/JS/pages/personalcanva/filtrarFavoritado.js
@@ -1,13 +1,16 @@
 import { resgatarECriarApenasFavoritos } from "../personalcanva/firestore/firestore.js";
 
 const secaoListaTermos = document.querySelector('#lista-termos');
-const btnMeusFavoritos = document.querySelector('[data-filtrar_favorito]');
+const btnMeusFavoritos = document.querySelectorAll('[data-filtrar_favorito]');
 
-btnMeusFavoritos.addEventListener('click', async () => {
-    try {
-        secaoListaTermos.innerHTML = '';
-        await resgatarECriarApenasFavoritos();
-    } catch (error) {
-        console.log(error)
-    }
-})
+btnMeusFavoritos.forEach((btn) => {
+    btn.addEventListener('click', async () => {
+        try {
+            secaoListaTermos.innerHTML = '';
+            await resgatarECriarApenasFavoritos();
+        } catch (error) {
+            console.log(error)
+        }
+    })
+});
+
