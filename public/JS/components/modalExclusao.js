3a1798f6fa67f98838f23305a5aa16f716a58677
Israel Nunes <israelnetonunes@gmail.com>
2025-04-04 10:37:41 -0300

    Fix - ajustar cores para dark mode do modal de edição e do modal de deletar

--- public/JS/components/modalExclusao.js
+++ public/JS/components/modalExclusao.js
@@ -10,10 +10,10 @@ function criarModalExclusao() {
     let textoBtnConfirmar = 'Confirmar';
 
     divPai.innerHTML = `
-        <div class="modal-box">
-            <p class="font-semibold">${mensagemConfirmacao}</p>
+        <div class="modal-box bg-branco-fundo">
+            <p class="text-preto-padrao font-semibold">${mensagemConfirmacao}</p>
             <div class="flex flex-row justify-end gap-x-4">
-                <button id="btn_cancelar-deletar_termo" class="btn" onclick="">${textoBtnCancelar}</button>
+                <button id="btn_cancelar-deletar_termo" class="btn text-preto-padrao bg-branco-fundo border-1 border-laranja-escuro" onclick="">${textoBtnCancelar}</button>
                 <button id="btn_confirmar-deletar_termo" onclick="" class="btn bg-gradient-to-r from-laranja-fraco to-laranja-medio text-branco-fundo hover:ring-1 hover:ring-laranja-medio">
                     ${textoBtnConfirmar}
                 </button>
